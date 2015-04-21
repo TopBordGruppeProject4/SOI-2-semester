f46f6dcdb44ccf94a5a715eb8364c3f01cf29316
Polenser <patrikfrederiksen@gmail.com>
2015-04-21 13:49:10 +0200

    Oprettede et par klasser

---
+++ 2 Semester Eksamensproject/1.Iteration/Model/Person.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _1.Iteration.Model
+{
+    public abstract class Person
+    {
+        public string Address { get; set; }
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Tlf { get; set; }
+    }
+}
---
+++ 2 Semester Eksamensproject/1.Iteration/Model/Worker.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _1.Iteration.Model
+{
+    public class Worker: Person
+    {
+        
+
+        public bool Admin { get; set; }
+        public string Password { get; set; }
+        public string Username { get; set; }
+
+        
+        
+    }
+}
