833fa199eaa6bbc3a47247cdec8fc5f9984c4f9b
Polenser <patrikfrederiksen@gmail.com>
2015-05-19 13:26:03 +0200

    Added lidt mere

--- 2 Semester Eksamensproject/SHI/ViewModel/DateTimeConverter.cs
+++ 2 Semester Eksamensproject/SHI/ViewModel/DateTimeConverter.cs
@@ -8,5 +8,9 @@ namespace SHI.ViewModel
 {
     class DateTimeConverter
     {
+        public static DateTime DateTimeOffsetAndTimeSetToDateTime(DateTimeOffset date, TimeSpan time)
+        {
+            return new DateTime(date.Year, date.Month, date.Day, time.Hours, time.Minutes, 0);
+        }
     }
 }
